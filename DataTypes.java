05a6aa268a9f44a8a64bc61526a2f8855fa7c77f
adhietya <rafflieadhietya13@gmail.com>
2021-05-24 14:59:05 +0700

    deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+ String namaDepan = "Muhammad";
+ String namaTengah = "Adhietya";
+ String namaBelakang = "Rafflie";
+ int usia = 20;
+ int targetTahunKuliah = 3;
+ double ipk = 3.88888888;
+ char nilaiAbjad = 'A';
+ boolean tampan = true;
+
+ }
+}
